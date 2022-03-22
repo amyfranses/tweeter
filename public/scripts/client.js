06749438a479988a4f016ff054020a47e022b54d
amyfranses <amyfranses@gmail.com>
2022-03-22 21:18:01 +0000

    add ajax get and post for new tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,7 +1,8 @@
 function createTweetElement(tweetObject) {
   let $tweet = tweetObject.content.text;
-  let article = $("<article>").addClass("tweet");
-  const postedTweet = `
+
+  const postedTweet = $(`
+        <article class="tweet">
           <header class="image-name-handle">
             <div class="image-name">
               <img class="user-image" src=${tweetObject.user.avatars}>
@@ -18,16 +19,14 @@ function createTweetElement(tweetObject) {
               <i class="fa-solid fa-retweet"></i>
               <i class="fa-solid fa-flag"></i>
             </div>   
-          </footer>          
-     `;
-  return article.append(postedTweet);
+          </footer>   
+          </article>       
+     `);
+  return postedTweet;
 }
 
 const renderTweets = function (tweets) {
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
-  $("#tweet-container").empty();
+  // $("#tweet-container").empty();
   for (let tweet of tweets) {
     $("#tweet-container").prepend(createTweetElement(tweet));
   }
@@ -45,33 +44,38 @@ const tweetObject = {
   created_at: 1461116232227,
 };
 
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
-
 const $tweet = createTweetElement(tweetObject);
+
+function loadTweets() {
+  $.ajax({
+    method: "GET",
+    dataType: "json",
+    url: "/tweets",
+    success: function (response) {
+      renderTweets(response);
+    },
+    error: (error) => console.log(error),
+  });
+}
+
 $(document).ready(() => {
-  renderTweets(data);
+  $("#new-tweet-form").on("submit", function (event) {
+    event.preventDefault();
+
+    $.ajax({
+      method: "POST",
+      url: "/tweets",
+      type: "application/json",
+      data: $(this).serialize(),
+      success: function () {
+        $("textarea").val("");
+        $.get("/tweets", (response) => {
+          const lastTweet = [response.slice(-1).pop()];
+          renderTweets(lastTweet);
+        });
+      },
+    });
+  });
+
+  loadTweets();
 });
-console.log($tweet);
