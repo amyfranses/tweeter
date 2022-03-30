6b67a806a19d4870ddbdefbb3e8911b729536c3b
amyfranses <amyfranses@gmail.com>
2022-03-30 19:18:05 +0000

    clean up code, remove console.logs

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -33,20 +33,6 @@ const renderTweets = function (tweets) {
   }
 };
 
-// const tweetObject = {
-//   user: {
-//     name: "Newton",
-//     avatars: "https://i.imgur.com/73hZDYK.png",
-//     handle: "@SirIsaac",
-//   },
-//   content: {
-//     text: "If I have seen further it is by standing on the shoulders of giants",
-//   },
-//   created_at: 1461116232227,
-// };
-
-// const $tweet = createTweetElement(tweetObject);
-
 function loadTweets() {
   $.ajax({
     method: "GET",
@@ -62,7 +48,6 @@ function loadTweets() {
 $(document).ready(() => {
   $("#new-tweet-form").on("submit", function (event) {
     event.preventDefault();
-    //  $("#error-message1").hide();
 
     const textinput = $("#tweet-text").val().trim().length;
 
@@ -86,7 +71,6 @@ $(document).ready(() => {
         data: $(this).serialize(),
         success: function () {
           $.get("/tweets", (response) => {
-            // const lastTweet = [response.slice(-1).pop()];
             renderTweets(response);
             $("#new-tweet-form")[0].reset();
             $(".counter").val(140);
